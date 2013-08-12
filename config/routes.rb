af425f907d5cb3a6e8fd58566e1c4b5dc0bcf2bb
Denny Mueller <denny@dennymueller.de>
2013-08-12 09:24:55 +0200

    change routes for handling app and frontpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Devel::Application.routes.draw do
   get "welcome/index"
-  root :to => "home#index"
+  # root :to => "home#index"
+  root to: 'home#index', constraints: lambda { |r| r.env['warden'].authenticate? }
+  root to: 'welcome#index'
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
 end
