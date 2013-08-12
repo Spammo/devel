c0169539a60ce723e9c41c58c2ec9e6a035101b2
Denny Mueller <denny@dennymueller.de>
2013-08-12 19:29:24 +0200

    empty?

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Devel::Application.routes.draw do
-	constraints lambda { |req| !req.session[:user_id].blank? } do
-		root :to => "home#index", :as => "dashboard"
+	#constraints lambda { |req| !req.session[:user_id].blank? } do
+		#root :to => "home#index", :as => "dashboard"
+	#end
+	authenticated :user do
+  	root :to => 'home#index', as: :authenticated_user
 	end
-  root to: 'welcome#index'
+	root :to => 'welcome#index'
+  #root to: 'welcome#index'
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
 end
