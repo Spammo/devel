bef227fea58cbb3abc26647493ba74db27741119
Denny Mueller <denny@dennymueller.de>
2013-08-13 17:12:48 +0200

    change tabwidth

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Devel::Application.routes.draw do
-	#constraints lambda { |req| !req.session[:user_id].blank? } do
-		#root :to => "home#index", :as => "dashboard"
-	#end
-	authenticated :user do
-  	root :to => 'home#index', as: :authenticated_user
-	end
-	root :to => 'welcome#index'
+  #constraints lambda { |req| !req.session[:user_id].blank? } do
+    #root :to => "home#index", :as => "dashboard"
+  #end
+  authenticated :user do
+    root :to => 'home#index', as: :authenticated_user
+  end
+  root :to => 'welcome#index'
   #root to: 'welcome#index'
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
